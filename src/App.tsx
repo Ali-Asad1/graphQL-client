7ceb9001ca96cd8914ca49d741854116b61af84f
Ali Asad <aliasad.3128@gmail.com>
2024-03-11 16:28:32 +0330

    feat: Integrate `Layout`, `PostList`, and `Loader` components into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import Layout from "./components/layouts/layout";
+import PostList from "./components/postList";
+import Loader from "./components/ui/loader";
+import { usePosts } from "./hooks/usePosts";
+
 const App = () => {
-  return <div>App</div>;
+  const { data, error, isError, isLoading } = usePosts();
+
+  return (
+    <Layout>
+      {isLoading ? (
+        <div className="flex w-full justify-center items-center">
+          <Loader />
+        </div>
+      ) : isError ? (
+        <p>{error}</p>
+      ) : (
+        <PostList posts={data} />
+      )}
+    </Layout>
+  );
 };
 export default App;
